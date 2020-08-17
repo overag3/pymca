8a5926726850a578ceb3dcb5dfaa4547e0facab0
Nicolas Rogier <nicolas.rogier@univ-lorraine.fr>
2020-08-17 15:34:24 +0200

    initial commit

---
+++ tools/chocolateyinstall.ps1
@@ -0,0 +1,46 @@
+﻿$ErrorActionPreference = 'Stop'
+$url64 = 'https://sourceforge.net/projects/pymca/files/pymca/PyMca5.5.5/pymca5.5.5-win64.exe/download'
+$version = [version]'5.5.5'
+
+$packageArgs = @{
+    packageName    = $env:ChocolateyPackageName
+    fileType       = 'EXE'
+    url64bit       = $url64
+    softwareName   = 'pymca*'
+    checksum64     = '95314221E180826B765E9D7F43634B0E4D218BAA4DFB5562BF90706AE190E770'
+    checksumType64 = 'sha256'
+    silentArgs     = '/S'
+    validExitCodes = @(0)
+}
+
+#Uninstalls the previous version of PyMca if either version exists
+Write-Output "Searching if the previous version exists..."
+
+[array]$checkreg = Get-UninstallRegistryKey -SoftwareName $packageArgs['softwareName']
+
+if ($checkreg.Count -eq 0) {
+    Write-Output 'No installed old version. Process to install PyMca.'
+    # No version installed, process to install
+    Install-ChocolateyPackage @packageArgs
+} elseif ($checkreg.count -ge 1) {
+    $checkreg | ForEach-Object {
+        if ($null -ne $_.PSChildName) {
+            if ([version]$_.DisplayVersion -lt $version) {
+                Write-Output "Uninstalling PyMca previous version : $($_.DisplayVersion)"
+                $packageArgs['file'] = "$($_.UninstallString)"
+
+                # Uninstall previous version
+                Uninstall-ChocolateyPackage @packageArgs
+        
+
+                # Process to install
+                Write-Output "Installing new version of PyMca"
+                Install-ChocolateyPackage @packageArgs
+            } elseif (([version]$_.DisplayVersion -eq $version) -and ($env:ChocolateyForce)) {
+                Write-Output "PyMca $version already installed, but --force option is passed, download and install"
+                Install-ChocolateyPackage @packageArgs
+            }
+        }
+    }
+}
+
